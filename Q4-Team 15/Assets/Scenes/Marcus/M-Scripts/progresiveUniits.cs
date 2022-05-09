a1595269446de9aa46e94d67e87c9864bcc78af8
bagelyboi5 <garmanmt@gmail.com>
2022-05-08 21:07:37 -0700

    e

--- Q4-Team 15/Assets/Scenes/Marcus/M-Scripts/progresiveUniits.cs
+++ Q4-Team 15/Assets/Scenes/Marcus/M-Scripts/progresiveUniits.cs
@@ -10,9 +10,9 @@ public class progresiveUniits : MonoBehaviour
     public int TankKilled;
     private GameObject BarracksBuilding;
     public bool Barracks;
-    public AudioClip Phase1;
-    public AudioClip Phase2;
-    public AudioClip Phase3;
+    public GameObject Phase1;
+    public GameObject Phase2;
+    public GameObject Phase3;
 
     void Start()
     {
@@ -29,15 +29,22 @@ public class progresiveUniits : MonoBehaviour
         }
         if (SolKilled >= 20)
         {
-            GameObject.Find("Music").GetComponent<AudioSource>().clip = Phase2;
-            GameObject.Find("Music").GetComponent<AudioSource>().Play();
             PManager.barracksUnits1.SetActive(true);
         }
         if (TankKilled >= 5)
         {
-            GameObject.Find("Music").GetComponent<AudioSource>().clip = Phase3;
-            GameObject.Find("Music").GetComponent<AudioSource>().Play();
             PManager.barracksUnits2.SetActive(true);
+        }
+        if (SolKilled >= 20 && TankKilled <= 5)
+        {
+            Phase2.SetActive(true);
+            Phase1.SetActive(false);
+        }
+        if (SolKilled >= 20 && TankKilled >= 5)
+        {
+            Phase1.SetActive(false);
+            Phase2.SetActive(false);
+            Phase3.SetActive(true);
         }
     }
 }
