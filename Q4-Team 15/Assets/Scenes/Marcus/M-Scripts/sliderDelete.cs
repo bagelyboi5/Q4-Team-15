34b32a794a51bbba4e9e52827f6d6136db1ee690
bagelyboi5 <garmanmt@gmail.com>
2022-05-09 23:37:20 -0700

    e

---
+++ Q4-Team 15/Assets/Scenes/Marcus/M-Scripts/sliderDelete.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderDelete : MonoBehaviour
+{
+
+    public int sliderVAL;
+    public int slid;
+    public int max;
+    private GameObject player;
+    public int sol;
+    public int tank;
+    public bool Tanktype;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        slid = sliderVAL;
+        sol = player.GetComponent<progresiveUniits>().SolKilled;
+        tank = player.GetComponent<progresiveUniits>().TankKilled;
+        gameObject.GetComponent<Slider>().value = slid;
+
+
+        if (Tanktype == true)
+        {
+            sliderVAL = tank;
+        }
+        else
+        {
+            sliderVAL = sol;
+        }
+
+        if (sliderVAL >= max)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
